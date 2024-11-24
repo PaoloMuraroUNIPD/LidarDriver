873e049893d920c37e10d783558099d194e94e1b
federicomaniglio <fmaniglio@itisrossi.vi.it>
2024-11-24 22:23:26 +0100

    Sistemata sintassi nel file LidarDriver.cpp Aggiunto piccola funzione main sul file main.cpp per testare funzionamento di linking della libreria

--- src/LidarDriver.cpp
+++ src/LidarDriver.cpp
@@ -4,65 +4,120 @@
 
 #include "../include/LidarDriver.h"
 
-class LidarDriver {
+//
+//class LidarDriver {
+//
+//    private:
+//
+//        double resolution;
+//        std::vector<std::vector<double>> buffer;
+//
+//        //dimensinoe massima del buffer
+//        size_t size;
+//        //numero di scansioni nel buffer
+//        size_t count;
+//        //indice scansione, punta all ultima scansione del lidar
+//        size_t index;
+//
+//        /* boolean isEmpty() { return count == 0 } */
+//
+//    public:
+//
+//        LidarDriver() = delete;
+//
+//        LidarDriver(double res, size_t buffer_dim) {
+//
+//            resolution = res;
+//            size = buffer_dim;
+//            count = 0;
+//            index = 0;
+//        }
+//
+//        void new_scan(const std::vector<double>& scan) {
+//
+//            /* parte alta di 181 (numero di scansioni a 1) moltiplicato per la risoluzione,
+//            il numero di scansioni che ci aspettiamo di rcevere dallo scan */
+//            true_size = (181/resolution) + 1;
+//
+//            // se la scansione ha un numero minore di dati della size prevista completo con 0
+//            if (scan.size() < true_size) { scan.resize(true_size, 0); }
+//            //se la scansione ha un numero maggiore di dati taglio i dati mancanti
+//            if (scan.size() > true_size) { scan.resize(true_size);
+//
+//            //aggiungo il nuovo vettore
+//            if (index < (size - 1)) {
+//                //mette scan nel vettore buffer nella posizione dell' index sucessivo
+//                index = index + 1;
+//                //dovrebbe mettere tutti i valori della scan in buffer[(index + 1)]
+//                for (int i : scan) buffer[index].push_back(i);
+//            }
+//            else
+//            {
+//                index = 0;
+//                for (int i : scan) buffer[index].push_back(i);
+//            }
+//            //se il buffer è non gia pieno, incrementa count
+//            if (count < size) { count = count + 1; }
+//        }
+//
+//        //elimina tutte le scan
+//        void clear_buffer(void) {
+//            buffer.clear();
+//            count = 0;
+//            index = 0;
+//        }
+//}
+LidarDriver::LidarDriver(double res, size_t buffer_dim) {
+    resolution = res;
+    size = buffer_dim;
+    count = 0;
+    index = 0;
 
-    private:
+}
 
-        double resolution;
-        std::vector<std::vector<double>> buffer;
+void LidarDriver::new_scan(const std::vector<double> &scan) {
+//              PARTE SCRITTA DA ALBERTO
+//            /* parte alta di 181 (numero di scansioni a 1) moltiplicato per la risoluzione,
+//            il numero di scansioni che ci aspettiamo di rcevere dallo scan */
+//            true_size = (181/resolution) + 1;
+//
+//            // se la scansione ha un numero minore di dati della size prevista completo con 0
+//            if (scan.size() < true_size) { scan.resize(true_size, 0); }
+//            //se la scansione ha un numero maggiore di dati taglio i dati mancanti
+//            if (scan.size() > true_size) { scan.resize(true_size);
+//
+//            //aggiungo il nuovo vettore
+//            if (index < (size - 1)) {
+//                //mette scan nel vettore buffer nella posizione dell' index sucessivo
+//                index = index + 1;
+//                //dovrebbe mettere tutti i valori della scan in buffer[(index + 1)]
+//                for (int i : scan) buffer[index].push_back(i);
+//            }
+//            else
+//            {
+//                index = 0;
+//                for (int i : scan) buffer[index].push_back(i);
+//            }
+//            //se il buffer è non gia pieno, incrementa count
+//            if (count < size) { count = count + 1; }
+//        }
+}
 
-        //dimensinoe massima del buffer
-        size_t size;
-        //numero di scansioni nel buffer
-        size_t count;
-        //indice scansione, punta all ultima scansione del lidar
-        size_t index;
+std::vector<double> LidarDriver::get_scan() {
+    return std::vector<double>();
+}
 
-        /* boolean isEmpty() { return count == 0 } */
-
-    public:
-
-        LidarDriver() = delete;
-
-        LidarDriver(double res, size_t buffer_dim) {
-
-            resolution = res;
-            size = buffer_dim;
-            count = 0;
-            index = 0;
-        }
-
-        void new_scan(const std::vector<double>& scan) {
-
-            /* parte alta di 181 (numero di scansioni a 1) moltiplicato per la risoluzione,
-            il numero di scansioni che ci aspettiamo di rcevere dallo scan */
-            true_size = (181/resolution) + 1;
-
-            // se la scansione ha un numero minore di dati della size prevista completo con 0
-            if (scan.size() < true_size) { scan.resize(true_size, 0); }
-            //se la scansione ha un numero maggiore di dati taglio i dati mancanti
-            if (scan.size() > true_size) { scan.resize(true_size);
-                                          
-            //aggiungo il nuovo vettore
-            if (index < (size - 1)) {
-                //mette scan nel vettore buffer nella posizione dell' index sucessivo
-                index = index + 1;
-                //dovrebbe mettere tutti i valori della scan in buffer[(index + 1)]
-                for (int i : scan) buffer[index].push_back(i);
-            }
-            else 
-            { 
-                index = 0;
-                for (int i : scan) buffer[index].push_back(i);
-            }
-            //se il buffer è non gia pieno, incrementa count 
-            if (count < size) { count = count + 1; }
-        }
-
-        //elimina tutte le scan
-        void clear_buffer(void) {
+void LidarDriver::clear_buffer() {
             buffer.clear();
             count = 0;
             index = 0;
-        }  
+}
+
+double LidarDriver::get_distance(double angle) {
+    return 0;
+}
+
+std::ostream &operator<<(std::ostream &os, const LidarDriver &lidar) {
+    os <<"Ciao";
+    return os;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,14 @@
 // main per testare la classe
+
+#include <iostream>
+
+#include "../include/LidarDriver.h"
+
+int main(void) {
+//    Serve un costruttore generico?
+    LidarDriver lidarDriver(1,10);
+    lidarDriver.clear_buffer();
+    std::cout<<lidarDriver<<std::endl;
+    std::cout << "OK, LET'S GO" << std::endl;
+    return 0;
+}
